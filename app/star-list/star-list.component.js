2252a179a59f64c65f00085ba85b0ab15f46c2a6
LIHTU <robinanelson@gmail.com>
2016-12-02 23:41:16 -0800

    added photos

--- app/star-list/star-list.component.js
+++ app/star-list/star-list.component.js
@@ -5,58 +5,13 @@ angular.
   module('starList').
   component('starList', {
     templateUrl: 'star-list/star-list.template.html',
-    controller: function starListController() {
-      this.stars = [
-        {
-          "StarID": 46533,
-          "Hip": 63608,
-          "HD": 113226,
-          "HR": 4932,
-          "Gliese": "",
-          "BayerFlamsteed": "47Eps Vir",
-          "ProperName": "Vindemiatrix",
-          "RA": 13.03632237,
-          "Dec": 10.95910186,
-          "Distance": 31.34796238,
-          "Mag": 2.85,
-          "AbsMag": 0.368953415,
-          "Spectrum": "G8IIIvar",
-          "ColorIndex": 0.934
-        },
-        {
-          "StarID": 65795,
-          "Hip": 91262,
-          "HD": 172167,
-          "HR": 7001,
-          "Gliese": "Gl 721",
-          "BayerFlamsteed": "3Alp Lyr",
-          "ProperName": "Vega",
-          "RA": 18.61560722,
-          "Dec": 38.78299311,
-          "Distance": 7.756146746,
-          "Mag": 0.03,
-          "AbsMag": 0.581769913,
-          "Spectrum": "A0Vvar",
-          "ColorIndex": -0.001
-        },
-        {
-          "StarID": 2671,
-          "Hip": 3829,
-          "HD": "",
-          "HR": "",
-          "Gliese": "Gl  35",
-          "BayerFlamsteed": "",
-          "ProperName": "Van Maanen's Star",
-          "RA": 0.81921585,
-          "Dec": 5.39519773,
-          "Distance": 4.406256885,
-          "Mag": 12.37,
-          "AbsMag": 14.14965094,
-          "Spectrum": "DG",
-          "ColorIndex": 0.554
-        }
-      ];
+    controller: function StarListController($http) {
+      // note: built-in angular services are prefixed with '$'; denotes a namespace.
+      var self = this;
+      self.orderProp = 'Distance';
 
-      this.orderProp = 'Distance';
+      $http.get('stars/stars.json').then(function(response) {
+        self.stars = response.data;
+      });
     }
   });
--- app/star-list/star-list.component.spec.js
+++ app/star-list/star-list.component.spec.js
@@ -7,18 +7,94 @@ describe('starList', function() {
 
   // Test the controller
   describe('StarListController', function() {
-    var ctrl;
+    var $httpBackend, ctrl;
 
-    beforeEach(inject(function($componentController) {
+    // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+    // This allows us to inject a service and assign it to a variable with the same name
+    // as the service while avoiding a name conflict.
+    beforeEach(inject(function($componentController, _$httpBackend_) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('stars/stars.json')
+                  .respond([
+                    {
+                      "StarID": 55587,
+                      "Hip": 77070,
+                      "HD": 140573,
+                      "HR": 5854,
+                      "Gliese": "Gl 596.2",
+                      "BayerFlamsteed": "24Alp Ser",
+                      "ProperName": "Unukalhai",
+                      "RA": 15.7377766,
+                      "Dec": 6.42551971,
+                      "Distance": 22.45172878,
+                      "Mag": 2.63,
+                      "AbsMag": 0.873751063,
+                      "Spectrum": "K2III",
+                      "ColorIndex": 1.167
+                    },
+                    {
+                      "StarID": 70401,
+                      "Hip": 97278,
+                      "HD": 186791,
+                      "HR": 7525,
+                      "Gliese": "",
+                      "BayerFlamsteed": "50Gam Aql",
+                      "ProperName": "Tarazed",
+                      "RA": 19.77099171,
+                      "Dec": 10.61326869,
+                      "Distance": 141.2429379,
+                      "Mag": 2.72,
+                      "AbsMag": -3.029833712,
+                      "Spectrum": "K3II",
+                      "ColorIndex": 1.507
+                    }
+                  ]);
+
       ctrl = $componentController('starList');
     }));
 
-    it('should create a `stars` model with 3 stars', function() {
-      expect(ctrl.stars.length).toBe(3);
+    it('should create a `stars` property with 2 stars fetched with `$http`', function() {
+      expect(ctrl.stars).toBeUndefined();
+
+      $httpBackend.flush();
+      expect(ctrl.stars).toEqual([
+        {
+          "StarID": 55587,
+          "Hip": 77070,
+          "HD": 140573,
+          "HR": 5854,
+          "Gliese": "Gl 596.2",
+          "BayerFlamsteed": "24Alp Ser",
+          "ProperName": "Unukalhai",
+          "RA": 15.7377766,
+          "Dec": 6.42551971,
+          "Distance": 22.45172878,
+          "Mag": 2.63,
+          "AbsMag": 0.873751063,
+          "Spectrum": "K2III",
+          "ColorIndex": 1.167
+        },
+        {
+          "StarID": 70401,
+          "Hip": 97278,
+          "HD": 186791,
+          "HR": 7525,
+          "Gliese": "",
+          "BayerFlamsteed": "50Gam Aql",
+          "ProperName": "Tarazed",
+          "RA": 19.77099171,
+          "Dec": 10.61326869,
+          "Distance": 141.2429379,
+          "Mag": 2.72,
+          "AbsMag": -3.029833712,
+          "Spectrum": "K3II",
+          "ColorIndex": 1.507
+        }
+      ]);
     });
 
-    it('should set a default value for the `orderProp` model', function() {
-      expect(ctrl.orderProp).toBe('age');
+    it('should set a default value for the `orderProp` property', function() {
+      expect(ctrl.orderProp).toBe('Distance');
     });
 
   });
